8428e8ae2d557a544a843371dd95cbd863cfc8da
DaisyOdongo <daisy.odongo@student.moringaschool.com>
2022-08-26 10:22:18 +0300

    commit tree 35d4ae14ec1acefccad68086ae5976f56fda8729

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const len = word.length;
+  for(let i = 0; i < len/2; i++){
+    if(word[i] !== word[len -1 -i]){
+      return false;
+    }
+  }
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
